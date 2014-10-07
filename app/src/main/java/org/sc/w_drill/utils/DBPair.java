1cf28141e3417ff85f03dde210fe56ae14314b42
CaptainVoronin <CaptainVoronin@gmail.com>
2014-10-08 01:19:19 +0400

    A new class file has been added

---
+++ app/src/main/java/org/sc/w_drill/utils/DBPair.java
@@ -0,0 +1,27 @@
+package org.sc.w_drill.utils;
+
+/**
+ * Created by Max on 10/8/2014.
+ */
+public class DBPair
+{
+    int id;
+    String value;
+
+    public DBPair ( int _id, String _value )
+    {
+        id = _id;
+        value = _value;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+}
