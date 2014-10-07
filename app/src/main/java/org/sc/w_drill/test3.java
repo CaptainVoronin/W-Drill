e8a48680a8f47204764d17a70e9363c562b713a2
CaptainVoronin <CaptainVoronin@gmail.com>
2014-10-07 16:01:13 +0400

    added trash files

--- app/src/main/java/org/sc/w_drill/test3.java
+++ app/src/main/java/org/sc/w_drill/test3.java
@@ -3,5 +3,5 @@ package org.sc.w_drill;
 /**
  * Created by MaxSh on 07.10.2014.
  */
-public class kjhkjhkjh {
+public class test3 {
 }
---
+++ app/src/main/java/org/sc/w_drill/utils/test.java
@@ -0,0 +1,7 @@
+package org.sc.w_drill.utils;
+
+/**
+ * Created by MaxSh on 07.10.2014.
+ */
+public class test {
+}
