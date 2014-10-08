1ee661b5efea06f092f7097138ba6484dbbc888a
CaptainVoronin <CaptainVoronin@gmail.com>
2014-10-09 00:03:52 +0400

    A new class which provide random word from a dictionary.

---
+++ app/src/main/java/org/sc/w_drill/db_wrapper/WordRandomizer.java
@@ -0,0 +1,54 @@
+package org.sc.w_drill.db_wrapper;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.sc.w_drill.db.WDdb;
+import org.sc.w_drill.dict.Dictionary;
+import org.sc.w_drill.dict.IWord;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Max on 10/8/2014.
+ */
+public class WordRandomizer
+{
+    WDdb database;
+    ArrayList<Integer> ids;
+    Dictionary dict;
+    int interval;
+    Random random;
+    public WordRandomizer( WDdb _db, Dictionary _dict )
+    {
+        ids = new ArrayList<Integer>();
+        database = _db;
+    }
+
+    public void init() throws ArrayIndexOutOfBoundsException
+    {
+        SQLiteDatabase db = database.getReadableDatabase();
+        String statement = "select id from words where dict_id = ?";
+        Cursor crs = db.rawQuery( statement, new String[] { Integer.valueOf( dict.getId() ).toString()} );
+
+        while( crs.moveToNext() )
+        {
+            ids.add( Integer.valueOf( crs.getInt( 0 ) ) );
+        }
+
+        crs.close();
+        db.close();
+
+        if( ids.size() == 0 || ids.size() == 1 )
+            throw new ArrayIndexOutOfBoundsException( );
+
+        random = new Random();
+    }
+
+    public IWord gerRandomWord()
+    {
+        int id = random.nextInt( ids.size()  ) - 1;
+        return DBWordFactory.getInstance( database, dict ).getWodEx( id );
+    }
+}
