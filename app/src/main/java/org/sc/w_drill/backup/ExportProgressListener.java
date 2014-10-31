327c2fb94e62615a7da2b556fedcfc6cb6658735
CaptainVoronin <CaptainVoronin@gmail.com>
2014-11-01 00:45:18 +0300

    A new class for backup asynk task params

---
+++ app/src/main/java/org/sc/w_drill/backup/ExportProgressListener.java
@@ -0,0 +1,10 @@
+package org.sc.w_drill.backup;
+
+/**
+ * Created by Max on 11/1/2014.
+ */
+public interface ExportProgressListener
+{
+    void setMaxValue( Integer max );
+    void setCurrentProgress( Integer current );
+}
